be78d8764b690fe42fb2c143f4ee2d53fe7a14e2
angusbhattarai <bhattaraiangus@gmail.com>
2024-08-28 22:55:09 +0545

    Addded passing by value

---
+++ src/PassingByValue/App.java
@@ -0,0 +1,46 @@
+package PassingByValue;
+
+public class App {
+    public static void main(String[] args) {
+        App app = new App();
+
+        //..........................................
+
+        int value = 7;
+        System.out.println("1. Value is: " + value);
+
+
+        app.show(value);
+        System.out.println("4. Value is: " + value);
+
+        //.........................................
+        System.out.println();
+
+        Person person = new Person("Angus");
+        System.out.println("1. Person is: " + person);
+
+        app.show(person);
+
+        System.out.println("4. Person is: " + person);
+    }
+
+    public void show(int value){
+        System.out.println("2. Value is: " + value);
+
+        value = 8;
+        System.out.println("3. Value is: " + value);
+
+    }
+
+    public void show(Person person){
+        System.out.println("2. Person is: " + person);
+
+        person.setName("Robert");
+
+        person = new Person("Abhram");
+        System.out.println("3. Person is:" + person);
+
+    }
+
+
+}
